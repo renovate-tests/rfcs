fa2e4ee719f6c9b48ccafbd512257ea18c7afd99
shsssskn <shsssskn@gmail.com>
2018-03-11 00:04:14 +0900

    Add Babel

---
+++ webpack4/.babelrc.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+
+const presets = ['@babel/flow', '@babel/react'];
+const plugins = ['@babel/plugin-syntax-class-properties', ['@babel/plugin-proposal-class-properties', {loose: true}]];
+
+if (env === 'local') {
+  presets.push.apply(presets, [
+    ['@babel/preset-stage-1', {loose: true}],
+    [('@babel/env', {loose: true, modules: false, useBuiltIns: 'usage'})],
+  ]);
+  plugins.push.apply(plugins, ['react-hot-loader/babel']);
+}
+
+if (env === 'test') {
+  presets.push.apply(presets, [
+    '@babel/preset-stage-1',
+    [
+      '@babel/env',
+      {
+        targets: {node: '8.9'},
+        modules: false,
+        useBuiltIns: 'usage',
+      },
+    ],
+  ]);
+}
+
+if (env === 'develop') {
+  presets.push.apply(presets, [
+    '@babel/preset-stage-1',
+    [
+      '@babel/env',
+      {
+        targets: {
+          browsers: ['> 1% in JP'],
+          modules: false,
+          useBuiltIns: 'usage',
+        },
+      },
+    ],
+  ]);
+}
+
+if (env === 'production') {
+  presets.push.apply(presets, [
+    '@babel/preset-stage-1',
+    [
+      '@babel/env',
+      {
+        targets: {
+          browsers: ['> 1% in JP'],
+          modules: false,
+          useBuiltIns: 'usage',
+        },
+      },
+    ],
+  ]);
+}
+
+module.exports = {presets, plugins};
