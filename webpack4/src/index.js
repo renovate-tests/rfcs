39d08625f769f96174ee7566e5f56f06fa19835d
shsssskn <shsssskn@gmail.com>
2018-03-11 20:32:11 +0900

    Add sources

---
+++ webpack4/src/index.js
@@ -0,0 +1,10 @@
+/* @flow */
+
+// external
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Hello My App!!</div>,
+  window.document.getElementsByClassName('___main')[0]
+);
